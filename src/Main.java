a814e0d0085869950a0a5329e7810282d6732744
aikezhanakhmatova <alinaakhmatova77@gmail.com>
2023-04-11 14:35:36 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        /// TODO 1,2,3 KONSOLGO CHYGARUU  ///  TAPSHYRMA
+
+//        Scanner scan = new Scanner(System.in);
+//        int nam = scan.nextInt();
+//        int num = scan.nextInt();
+//        int nul = scan.nextInt();
+//        System.out.println(nam+"," + num + "," + nul +",");
+
+        //// TODO RANDOM LOGICHESKIE OPERATOPY ///
+
+//        Scanner sc = new Scanner(System.in);
+//        int nam = sc.nextInt();
+//        Random random = new Random();
+//        int number = random.nextInt(1,5);
+//        if(number==5){
+//            System.out.println(number+"=="+"барабар");
+//        }else {
+//            System.out.println(number +"!=" +nam);
+//        }
+
+        /// TODO EVEN NOT EVEN NBVBER  ///
+
+//        Scanner scan = new Scanner(System.in);
+//        int number = scan.nextInt();
+//        if(number%2==0) {
+//            System.out.println("Жуп сан");
+//        }else if(number%2==1){
+//            System.out.println("Так сан");
+//        }
+
+
+    }
+}
